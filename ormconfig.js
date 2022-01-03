fc96d88ccfac3fa7815e53d56ee0800f7269c853
Mateus Nicolau <mateus2017nicolau@gmail.com>
2022-01-03 10:06:55 -0300

    add engine for switch for dev or production

---
+++ ormconfig.js
@@ -0,0 +1,26 @@
+
+const DEV = (process.argv[2] !== 'dev' || process.argv[2] === undefined) ? false : true
+
+module.exports = {
+   "name": "default",
+   "type": "postgres",
+   "url": process.env.DATABASE_URL,
+   "synchronize": true,
+   "logging": false,
+   "entities": [
+      `${(DEV) ? "src/entity/index.ts" : "build/entity/index.js"}`
+   ],
+   "migrations": [
+      `${(DEV) ? "src/database/migration/**/*.ts" : "build/database/migration/**/*.js"}`
+   ],
+   "subscribers": [
+      `${(DEV) ? "src/subscriber/**/*.ts" : "build/subscriber/**/*.js"}`
+   ],
+   "cli": {
+      "entitiesDir": "src/entity",
+      "migrationsDir": "src/database/migration",
+      "subscribersDir": "src/subscriber"
+   }
+}
+
+
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
+import dotenv from 'dotenv'
 import app from './app'
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
 
 createConnection().then(sucess => {
+  dotenv.config()
   app.listen(process.env.port, () => { console.log("====>===>===>==>==>=>=> Running server.") })
 }).catch(error => {
-  console.log("Error in connected in database");
+  console.log(error)
+  console.log("Error in connected database");
 })
+
+
 
 
 
