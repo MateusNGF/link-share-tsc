f69077c4d5850b5877a0db7a5aaa0e6d367ded1b
Mateus Nicolau <mateus2017nicolau@gmail.com>
2022-01-05 16:18:53 -0300

    fix port change UPERCASE

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { createConnection } from 'typeorm'
 
 createConnection().then(sucess => {
   dotenv.config()
-  app.listen(process.env.port || 3000, () => { console.log("===>  ✅  Running server.  ✅  <===") })
+  app.listen(process.env.PORT || 3000, () => { console.log("===>  ✅  Running server.  ✅  <===") })
 }).catch(error => {
   console.log("Error in connected database");
 })
