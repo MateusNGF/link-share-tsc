1793c4a50dffb3fff81b951d41b32820636c01b0
Mateus Nicolau <mateus.nicolau@devmagic.com.br>
2022-01-14 19:28:56 -0300

    fix(*): switch name env port for PORT

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { createConnection } from 'typeorm'
 
 try {
   createConnection()
-  const port = process.env.port || 3000
+  const port = process.env.PORT || 3000
   dotenv.config()
   app.listen(port, () => {
     console.log("===>  ✅  Running server (" + port + ")  ✅  <===")
