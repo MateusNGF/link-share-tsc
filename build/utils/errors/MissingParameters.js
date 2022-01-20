8e97ffd5d30ff69dbd2991cbbca25c6b1d9000cc
Danrlei Hornke <contato@dscoding.com.br>
2022-01-19 22:20:55 -0300

    Delete build directory

--- build/utils/errors/MissingParameters.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MissingParameters extends Error {
-    constructor(msg) {
-        super(msg);
-        this.name = "MissingParameters";
-    }
-}
-exports.MissingParameters = MissingParameters;
--- build/utils/errors/custom/ErrorCustom.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ErrorCustom extends Error {
-    constructor() {
-        super(...arguments);
-        this.custom = true;
-    }
-}
-exports.ErrorCustom = ErrorCustom;
