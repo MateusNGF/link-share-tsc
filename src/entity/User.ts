68c9140614e21a0c7d56ed1788bf050169b74193
Danrlei <contato@dscoding.com.br>
2022-02-06 16:26:24 -0300

    Update User.ts

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,55 +1,57 @@
-import Joi from 'joi'
-import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, Repository, UpdateDateColumn } from 'typeorm'
-import { Link } from '../entity'
-import { InvalidParam, schemas } from '../utils'
+import Joi from "joi";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Link } from "../entity";
+import { InvalidParam, schemas } from "../utils";
 
-@Entity('user')
+@Entity("user")
 export class User {
-
-  constructor(new_user: User = null) {
-    try {
-      Object.assign(this, new_user)
-    } catch (e) {
-      throw new InvalidParam(e.message)
+    constructor(new_user: User = null) {
+        try {
+            Object.assign(this, new_user);
+        } catch (e) {
+            throw new InvalidParam(e.message);
+        }
     }
-  }
 
-  @PrimaryGeneratedColumn()
-  id?: string
+    @PrimaryGeneratedColumn()
+    id?: string;
 
-  @Column({ nullable: false })
-  name: string
+    @Column({ nullable: false })
+    name: string;
 
-  @Column({ unique: true, nullable: false })
-  email: string
+    @Column({ unique: true, nullable: false })
+    email: string;
 
-  @Column({ unique: true, nullable: false })
-  nickname: string
+    @Column({ unique: true, nullable: false })
+    nickname: string;
 
-  @Column({ nullable: false })
-  password: string
+    @Column({ nullable: false })
+    password: string;
 
-  @Column({ nullable: true, default: null })
-  pic_profile?: string
+    @Column({ nullable: false, default: false })
+    verified: boolean;
 
-  @Column({ nullable: true, default: null, length: 500 })
-  description?: string
+    @Column({ nullable: true, default: null })
+    pic_profile?: string;
 
-  @OneToMany(() => Link, links => links.owner, { eager: true, cascade: true })
-  links?: Link[]
+    @Column({ nullable: true, default: null, length: 500 })
+    description?: string;
 
-  @CreateDateColumn({ name: 'created_At' })
-  createdAt?: Date
+    @OneToMany(() => Link, (links) => links.owner, { eager: true, cascade: true })
+    links?: Link[];
 
-  @UpdateDateColumn({ name: 'update_At' })
-  updateAt?: Date
+    @CreateDateColumn({ name: "created_At" })
+    createdAt?: Date;
 
-  isOwner(link: Link): boolean {
-    if (this.id.toString() === link.owner.toString()) return true
-    return false
-  }
+    @UpdateDateColumn({ name: "update_At" })
+    updateAt?: Date;
 
-  async valid(thisProps: string[] = null) {
-    return schemas.user.methods.validProps(thisProps || ["name", "nickname", "password", "email"], this)
-  }
+    isOwner(link: Link): boolean {
+        if (this.id.toString() === link.owner.toString()) return true;
+        return false;
+    }
+
+    async valid(thisProps: string[] = null) {
+        return schemas.user.methods.validProps(thisProps || ["name", "nickname", "password", "email"], this);
+    }
 }
