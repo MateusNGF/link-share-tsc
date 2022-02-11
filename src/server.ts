7b9453623f28fd66adb14798d4552fa6bad0e3fc
Danrlei <contato@dscoding.com.br>
2022-02-10 21:57:10 -0300

    :art: implementação validação de email

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,15 @@
-import dotenv from 'dotenv'
-import app from './app'
-import 'reflect-metadata'
-import { createConnection } from 'typeorm'
+import dotenv from "dotenv";
+import app from "./app";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 
 try {
-  createConnection()
-  const port = process.env.PORT || 3000
-  dotenv.config()
-  app.listen(port, () => {
-    console.log("===>  ✅  Running server (" + port + ")  ✅  <===")
-  })
+   dotenv.config();
+   createConnection();
+   const port = process.env.PORT || 3000;
+   app.listen(port, () => {
+      console.log("===>  ✅  Running server (" + port + ")  ✅  <===");
+   });
 } catch (e) {
-  console.log("Error in connected database");
+   console.log("Error in connected database");
 }
-
-
-
-
-
-
-
