b57e678530a2d49cb224c3784a0c8ffc323870d5
Danrlei Hornke <contato@dscoding.com.br>
2022-04-06 23:04:52 -0300

    Update user.route.ts
    change pic route to post method

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -14,6 +14,6 @@ userRouter.post("/validate", ExpressAdapterRouter.adapt(new ValidateEmail()));
 userRouter.get("/refresh", verify, ExpressAdapterRouter.adapt(new Refresh()));
 userRouter.put("/update", verify, ExpressAdapterRouter.adapt(new Update()));
 userRouter.get("/visitor/:nickname", ExpressAdapterRouter.adapt(new GetUser()))
-userRouter.put("/pic", verify, multer(new StoragePicProfile().config()).single("pic_profile"), (req, res) => {
+userRouter.post("/pic", verify, multer(new StoragePicProfile().config()).single("pic_profile"), (req, res) => {
    console.log(req.file);
 });
