d15356abecf306f6a22a4073dc51d687dfde92d2
Arthur Rocha <62079236+ArthurMaciel95@users.noreply.github.com>
2022-04-08 23:44:33 -0300

    alterando headers, permitindo novas origens

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import { verify } from './utils';
 const app = express()
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://www.linkshare.com.br, http://www.localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
