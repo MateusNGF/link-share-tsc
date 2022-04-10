0ff61e3bd61cfcb67c348e590c8a364a487d8257
Gomess <mateus.nicolau@devmagic.com.br>
2022-04-10 20:42:20 -0300

    feature(*): switch Email sender for class

--- src/utils/Email.ts
+++ src/utils/Email.ts
@@ -1,8 +1,40 @@
 import Joi from "joi";
+import { createTransport, Transporter } from "nodemailer";
+import * as Mail from "nodemailer/lib/mailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+
 
 export class Email {
 
+  constructor(
+    private readonly host: string = String(process.env.EMAIL_HOST),
+    private readonly port: number = Number(process.env.EMAIL_PORT!),
+    private readonly passsword: string = String(process.env.EMAIL_PASSWORD),
+    private readonly user:string = String(process.env.EMAIL_USER),
+    private transporter: Transporter<SMTPTransport.SentMessageInfo> = null
+  ) {
+    this.transporter = createTransport({
+      host: this.host,
+      port: this.port,
+      secure: false, //SSL /TLS
+      logger: false,
+      debug: false,
+      auth: {
+        user: this.user,
+        pass: this.passsword,
+      },
+    })
+  }
+
   static async isEmail(email: string): Promise<any> {
     return await Joi.string().email().validateAsync(email)
+  }
+
+
+  public async send(options : Mail.Options){
+    if (!options.from) {options.from = this.user}
+    await this.transporter.sendMail(options)
+    this.transporter.close()
+    return true
   }
 }
