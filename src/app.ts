dfde77bf0c0238bb9899bd961128080973a54bac
Arthur Rocha <62079236+ArthurMaciel95@users.noreply.github.com>
2022-04-09 23:08:51 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, Access-Control-Allow-Origin");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
