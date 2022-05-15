15630739fc3c281f2ee9ff99649c06bac0c4e450
Gomess <mateus.nicolau@devmagic.com.br>
2022-05-15 20:24:50 -0300

    fix: resolve vunerabilite in create user

--- src/controllers/user/Create.controller.ts
+++ src/controllers/user/Create.controller.ts
@@ -3,46 +3,67 @@ import { IController } from "..";
 import { Link, User, Validate } from "../../entity";
 import { v4 as uuid } from "uuid";
 import { UserRepository, ValidateRepository } from "../../repository";
-import { buildBody, InvalidParam, Messenger, typeCustomRequest, typeCustomResponse } from "../../utils";
+import {
+  buildBody,
+  InvalidParam,
+  Messenger,
+  typeCustomRequest,
+  typeCustomResponse,
+} from "../../utils";
 import { SendEmailValidateCode } from "../../utils/sendEmail/services";
 
 export class Create implements IController {
-   async exec(request: typeCustomRequest): Promise<typeCustomResponse> {
-      try {
-         const repository = getCustomRepository(UserRepository);
-         const validateRepository = getCustomRepository(ValidateRepository);
+  async exec(request: typeCustomRequest): Promise<typeCustomResponse> {
+    try {
+      const repository = getCustomRepository(UserRepository);
+      const validateRepository = getCustomRepository(ValidateRepository);
 
-         const userCurrent = new User(request.body);
-         // userCurrent.password = hashPassword(request.body.password);
+      const userCurrent = new User({
+        name: request.body.name,
+        email: request.body.email,
+        nickname: request.body.nickname,
+        password: request.body.password,
+        description: request.body.description,
+      });
 
-         await userCurrent.valid();
-         
-         userCurrent.nicknameFormatIsValid(userCurrent.nickname);
+      await userCurrent.valid();
 
-         userCurrent.nickname = `@${userCurrent.nickname}`
+      userCurrent.nicknameFormatIsValid(userCurrent.nickname);
 
-         if (request.body.links && request.body.links.length > 0) {
-            var promises = [];
-            userCurrent.links = [];
-            request.body.links.forEach(async (link: Link) => {
-               const linkCurrent = new Link(link);
-               // corrigir a validação de url
-               promises.push(linkCurrent.valid());
-               userCurrent.links.push(linkCurrent);
-            });
-            await Promise.all(promises).catch((e) => {
-               throw new InvalidParam(`${e.message} - (${e._original})`);
-            });
-         }
+      userCurrent.nickname = `@${userCurrent.nickname}`;
 
-         await repository.validCredentials(userCurrent.nickname, userCurrent.email);
-         const savedCurrentUser: User = await repository.save(userCurrent);
-         const savedValidate: Validate = await validateRepository.save({ uuid: uuid(), owner: savedCurrentUser });
-
-         await SendEmailValidateCode(savedValidate.owner.email, savedValidate.uuid, savedCurrentUser.name);
-         return Messenger.success(buildBody(savedCurrentUser));
-      } catch (error) {
-         return Messenger.error(error);
+      if (request.body.links && request.body.links.length > 0) {
+        var promises = [];
+        userCurrent.links = [];
+        request.body.links.forEach(async (link: Link) => {
+          const linkCurrent = new Link(link);
+          // corrigir a validação de url
+          promises.push(linkCurrent.valid());
+          userCurrent.links.push(linkCurrent);
+        });
+        await Promise.all(promises).catch((e) => {
+          throw new InvalidParam(`${e.message} - (${e._original})`);
+        });
       }
-   }
+
+      await repository.validCredentials(
+        userCurrent.nickname,
+        userCurrent.email
+      );
+      const savedCurrentUser: User = await repository.save(userCurrent);
+      const savedValidate: Validate = await validateRepository.save({
+        uuid: uuid(),
+        owner: savedCurrentUser,
+      });
+
+      await SendEmailValidateCode(
+        savedValidate.owner.email,
+        savedValidate.uuid,
+        savedCurrentUser.name
+      );
+      return Messenger.success(buildBody(savedCurrentUser));
+    } catch (error) {
+      return Messenger.error(error);
+    }
+  }
 }
